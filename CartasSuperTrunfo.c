945ebd9c0abf191be8210499c7e9c4ec05520ee3
João <joaolima1802@gmail.com>
2025-08-08 07:16:05 +0000

    Criada a base do desafio novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,35 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    char estado1, estado2, codigo1[4], codigo2[4], nome1[20], nome2[20];
+    int popul1, popul2, turis1, turis2;
+    float area1, area2, pib1, pib2;
+
+    printf("Seja bem vindo ao Super Trunfo: Países em C! :D\n\n");
+    printf("Neste Brasil fictício, teremos 8 estados e 4 cidades em cada estado.\n");
+    printf("Vamos adicionar duas cidades. Comece adicionando os dados da primeira carta!\n");
+
+    printf("Digite uma letra de A a H correspondente ao Estado: ");
+    scanf(" %c", &estado1);
+
+    printf("Agora insira o código da carta! O formato deve ser a letra escolhida para o estado e um número de 01 a 04 (A01, por ex): ");
+    scanf(" %s", codigo1);
+
+    printf("Boa escolha! E qual o nome dessa cidade? ");
+    scanf(" %s", nome1);
+
+    printf("Quantos habitantes ela possui? ");
+    scanf(" %i", &popul1);
+
+    printf("Qual sua área em km²? ");
+    scanf(" %f", &area1);
+
+    printf("Qual o PIB? ");
+    scanf(" %f", &pib1);
+
+    printf("Última característica! Quantos pontos turísticos ela possui? ");
+    scanf(" %i", &turis1);
 
     return 0;
 }
