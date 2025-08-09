32c66503f4771ef349dc44813df7a3fc08294b1d
João <joaolima1802@gmail.com>
2025-08-09 05:56:53 +0000

    Acrescido a exibição das cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -17,7 +17,9 @@ int main() {
     scanf(" %s", codigo1);
 
     printf("Boa escolha! E qual o nome dessa cidade? ");
-    scanf(" %s", nome1);
+    scanf(" %[^\n]", nome1);
+
+    getchar();
 
     printf("Quantos habitantes ela possui? ");
     scanf(" %i", &popul1);
@@ -31,14 +33,17 @@ int main() {
     printf("Última característica! Quantos pontos turísticos ela possui? ");
     scanf(" %i", &turis1);
     // dados da segunda carta
-    printf("Muito bem, agora vamos adicionar a segunda carta do nosso jogo.\n Digite a letra correspondente ao Estado: ");
+    printf("Muito bem, agora vamos adicionar a segunda carta do nosso jogo.\n");
+    printf("Digite a letra correspondente ao Estado: ");
     scanf(" %c", &estado2);
 
     printf("Agora o código da segunda carta: ");
     scanf(" %s", codigo2);
 
     printf("Qual o nome da cidade? ");
-    scanf(" %s", nome2);
+    scanf(" %[^\n]", nome2);
+
+    getchar();
 
     printf("Quantos habitantes residem nela? ");
     scanf(" %i", &popul2);
@@ -52,9 +57,24 @@ int main() {
     printf("Por fim, quantos pontos turísticos ela possui? ");
     scanf(" %i", &turis2);
 
+    // exibição das cartas
+    printf("\n Muito bem! Vamos ver as cartas cadastradas:\n");
+
+    printf("--------CARTA 1--------\n");
+    printf("CÓDIGO: %s - - ESTADO: %c \n", codigo1, estado1);
+    printf("- - %s - - \n", nome1);
+    printf("HAB: %i - - PIB: R$ %.2f \n", popul1, pib1);
+    printf("%.0fkm² - - %i pontos turísticos. \n \n", area1, turis1);
+
+    printf("--------CARTA 2--------\n");
+    printf("CÓDIGO: %s - - ESTADO: %c \n", codigo2, estado2);
+    printf("- - %s - - \n", nome2);
+    printf("HAB: %i - - PIB: R$ %.2f \n", popul2, pib2);
+    printf("%.0fkm² - - %i pontos turísticos. \n", area2, turis2);
+
     return 0;
 }
-// mestre, tentei fazer o código da forma mais limpa possível e de uma maneira que realmente parecesse uma interação com o usuário. o nome da cidade não aceita espaços,
-// pesquisei e vi que é necessário outro tipo de comando para isso mas não arrisquei tentar usá-lo pois as instruções da atividade pedem para manter as coisas
-// simples com os aprendizados do nível novato. confesso que estou incomodado do nome da cidade não aceitar espaços. era pra ficar assim mesmo? pode me dar um feedback?
-// obrigado!
+// mestre, tentei seguir todas as orientações da IA que responde feedback aqui no github, consegui deixar a entrada de dados do nome da cidade funcionando
+// corretamente com espaços, apenas não segui a orientação da IA de organizar o código das cartas em uma struct porque ainda não vi esse conteúdo (nível novato)
+// e estou mantendo o código simples e com os conteúdos que vimos até então, tal como foi orientado. Apenas me estendi um pouco com o scanf e getchar pra ler os
+// espaços nos nomes.
