151827b759d5467f4b22aafeb6553f292b67ee61
João <joaolima1802@gmail.com>
2025-08-13 06:29:54 +0000

    Adicionado os requisitos do nível aventureiro. Feito algumas alterações de melhoria no código.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,11 +2,12 @@
 
 int main() {
     // definição das variáveis
-    char estado1, estado2, codigo1[4], codigo2[4], nome1[20], nome2[20];
+    char estado1, estado2, codigo1[4], codigo2[4], cidade1_nome[50], cidade2_nome[50];
     int popul1, popul2, turis1, turis2;
-    float area1, area2, pib1, pib2;
+    float area1, area2, pib1, pib2, pibpc1, pibpc2, densip1, densip2; // pibpc = pib per capita; densip = densidade populacional
+
     // mensagem de boas-vindas e introdução
-    printf("Seja bem vindo ao Super Trunfo: Países em C! :D\n\n");
+    printf("| | | Seja bem vindo ao Super Trunfo: Países em C! :D | | |\n\n");
     printf("Neste Brasil fictício, teremos 8 estados e 4 cidades em cada estado.\n");
     printf("Vamos adicionar duas cidades. Comece adicionando os dados da primeira carta!\n");
     // dados da primeira carta
@@ -17,7 +18,7 @@ int main() {
     scanf(" %s", codigo1);
 
     printf("Boa escolha! E qual o nome dessa cidade? ");
-    scanf(" %[^\n]", nome1);
+    scanf(" %[^\n]", cidade1_nome);
 
     getchar();
 
@@ -41,7 +42,7 @@ int main() {
     scanf(" %s", codigo2);
 
     printf("Qual o nome da cidade? ");
-    scanf(" %[^\n]", nome2);
+    scanf(" %[^\n]", cidade2_nome);
 
     getchar();
 
@@ -57,24 +58,34 @@ int main() {
     printf("Por fim, quantos pontos turísticos ela possui? ");
     scanf(" %i", &turis2);
 
+    // cálculos de densidade populacional e pib per capita
+    pibpc1 = pib1 / (float) popul1;
+    pibpc2 = pib2 / (float) popul2;
+    densip1 = (float) popul1 / area1;
+    densip2 = (float) popul2 / area2;
+
     // exibição das cartas
     printf("\n Muito bem! Vamos ver as cartas cadastradas:\n");
 
     printf("--------CARTA 1--------\n");
     printf("CÓDIGO: %s - - ESTADO: %c \n", codigo1, estado1);
-    printf("- - %s - - \n", nome1);
-    printf("HAB: %i - - PIB: R$ %.2f \n", popul1, pib1);
-    printf("%.0fkm² - - %i pontos turísticos. \n \n", area1, turis1);
+    printf("- - %s - - \n", cidade1_nome);
+    printf("HAB: %i \n", popul1);
+    printf("PIB: R$ %.2f \n", pib1);
+    printf("%.0fkm² \n", area1);
+    printf("%i pontos turísticos. \n", turis1);
+    printf("PIB per Capita: R$%.2f \n", pibpc1);
+    printf("Densidade Populacional: %.2f hab/km² \n \n", densip1);
 
     printf("--------CARTA 2--------\n");
     printf("CÓDIGO: %s - - ESTADO: %c \n", codigo2, estado2);
-    printf("- - %s - - \n", nome2);
-    printf("HAB: %i - - PIB: R$ %.2f \n", popul2, pib2);
-    printf("%.0fkm² - - %i pontos turísticos. \n", area2, turis2);
+    printf("- - %s - - \n", cidade2_nome);
+    printf("HAB: %i \n", popul2);
+    printf("PIB: R$ %.2f \n", pib2);
+    printf("%.0fkm². \n", area2);
+    printf("%i pontos turísticos. \n", turis2);
+    printf("PIB per Capita: R$%.2f \n", pibpc2);
+    printf("Densidade Populacional: %.2f hab/km² \n \n", densip2);
 
     return 0;
 }
-// mestre, tentei seguir todas as orientações da IA que responde feedback aqui no github, consegui deixar a entrada de dados do nome da cidade funcionando
-// corretamente com espaços, apenas não segui a orientação da IA de organizar o código das cartas em uma struct porque ainda não vi esse conteúdo (nível novato)
-// e estou mantendo o código simples e com os conteúdos que vimos até então, tal como foi orientado. Apenas me estendi um pouco com o scanf e getchar pra ler os
-// espaços nos nomes.
